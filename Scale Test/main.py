8f6154a570adc45129e1b4d256811a7b3a0c25ca
MarkLebedev <ml220501@gmail.com>
2026-08-05 16:15:40 +0700

    Scale Test

--- Scale Test/main.py
+++ Scale Test/main.py
@@ -1,5 +1,29 @@
 import chem2ode
 import ode2jacobian
+import driver_config_generator
+import driver_constructor
+import traceback
 
-chem2ode.main()
-ode2jacobian.main()
+try:
+    input1 = input(
+        "type 1 to generate ode, jacobian and config files, type 2 to generate Livermore driver file (make sure the other files are present)\n")
+    if input1 == "1":
+        chem2ode.main()
+        ode2jacobian.main()
+
+        driver_config_generator.main()
+        input2 = input(
+            "type 1 to generate Livermore driver file (default config values), type 2 to stop (to edit config)\n")
+        if input2 == "1":
+            driver_constructor.main()
+        if input2 == "2":
+            pass
+    if input1 == "2":
+        driver_constructor.main()
+
+except Exception as e:
+    print(f"\n--- AN ERROR OCCURRED ---")
+    traceback.print_exc()
+
+print("Your program has finished running.")
+input("Press Enter to exit...")
--- Scale Test/ode2jacobian.py
+++ Scale Test/ode2jacobian.py
@@ -2,10 +2,11 @@ from chem2ode import fmult
 import re
 import time
 
+
 def main():
     ode = []
 
-    with open('ode.txt','r') as file:
+    with open('ode.txt', 'r') as file:
         for line in file:
             ode.append(line.strip('\n'))
 
@@ -14,7 +15,7 @@ def main():
     output_string = ''
     for i in range(len(ode)):
         for j in range(len(ode)):
-            target = f'Y({j+1})'
+            target = f'Y({j + 1})'
             line = ode[i].split(' ')
             output = ''
             for element in line:
@@ -23,13 +24,15 @@ def main():
                     if re.search(r'\d*D\d', element.split('*')[0]):
                         newcoeff = fmult(power, element.split('*')[0])
                         if newcoeff[0] != '-':
-                            newcoeff = '+'+newcoeff
-                        newelement = str(newcoeff) + element.replace(element.split('*')[0],'',1).replace(f'*{target}','',1)
+                            newcoeff = '+' + newcoeff
+                        newelement = str(newcoeff) + element.replace(element.split('*')[0], '', 1).replace(f'*{target}',
+                                                                                                           '', 1)
                     else:
-                        newelement = element.replace(f'*{target}','',1).replace(element.split('*')[1], element.split('*')[1]+f'*{power}')
+                        newelement = element.replace(f'*{target}', '', 1).replace(element.split('*')[1],
+                                                                                  element.split('*')[1] + f'*{power}')
                     output += newelement + ' '
             if len(output) != 0:
-                output_string += f'PD({i+1},{j+1}) = {output}'
+                output_string += f'PD({i + 1},{j + 1}) = {output}'
                 output_string += '\n'
 
     end_time = time.perf_counter()
@@ -39,4 +42,4 @@ def main():
     with open('jacobian.txt', 'w') as f:
         f.write(output_string)
 
-    print(f"Execution time: {execution_time:.4f} seconds")
+    print(f"ode2jacobian execution time: {execution_time:.4f} seconds")
